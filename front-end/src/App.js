afed047a973c360a818f6e1f0330bb3eef36ca9c
bisrat <bisrat.amtataw@gmail.com>
2022-06-29 23:28:47 -0400

    Footer test

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Nav from '../src/components/Nav'
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
       <Nav />
       <h1>hello</h1>
-
+      <Footer />
     </div>
   );
 }
