75c7475ea7afeb593943df9a63c60720a761a0d9
bisrat <bisrat.amtataw@gmail.com>
2022-06-30 22:41:59 -0400

    test database

---
+++ Back-end/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const mongoose= require('mongoose')
+
+const app = express()
+
+require('./config/db.connection')
+
+app.listen(process.env.PORT, ()=> console.log(`Listening in port: $ {process.env.PORT}`))
